7949f86dc497c17c5594af3e53623acfd61d4258
teenarr <tina.chan91@gmail.com>
2019-02-26 22:38:49 -0700

    corrected footer and edited package.json

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="page-footer font-small">
       <div className="footer-copyright text-center py-3">
         <span className="text-muted">Clicky Game! | Copyright &copy; Tina Li 2019 | 
-          <a href="https://github.com/tinali91/mongoose-scraper" target="blank">GitHub Repository</a> | 
+          <a href="https://github.com/tinali91/clickyGame" target="blank">GitHub Repository</a> | 
           <a href="https://tinali91.github.io/updatedPortfolio/" target="blank">Portfolio</a>
         </span>
       </div>
